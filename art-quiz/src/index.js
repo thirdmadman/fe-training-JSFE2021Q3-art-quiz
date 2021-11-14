cec3a9fe448f1f5f0d4702e49d617e85ef08a95d
thirdmadman <thirdmadman@gmail.com>
2021-11-14 22:42:56 +0400

    feat: add whois game layout

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -1,6 +1,31 @@
-import "./scss/style.scss";
+import './scss/style.scss';
 //import "./js/srcremaker.js"
 
-document.body.addEventListener('click', () => {
-  document.getElementsByClassName("sidebar")[0].classList.toggle("sidebar_hidden");
-}, true); 
+document.getElementsByClassName('burger-menu')[0].addEventListener('click', () => {
+  document.getElementsByClassName('sidebar')[0].classList.remove('sidebar_hidden');
+});
+
+document.getElementsByClassName('button-close')[0].addEventListener('click', () => {
+  document.getElementsByClassName('sidebar')[0].classList.add('sidebar_hidden');
+});
+
+// document.addEventListener('click', () => {
+//   document.getElementsByClassName("game-qestion-page")[0].classList.toggle("game-qestion-page_hidden");
+// }, true);
+
+[...document.getElementsByClassName('qestion-card_whois')[0].getElementsByClassName('qestion-card_answers-grid')].forEach((element) => {
+  element.addEventListener('click', () => {
+    document.getElementsByClassName('overlay')[0].classList.toggle('overlay_hidden');
+  });
+});
+
+document.getElementsByClassName('qestion-popup__button')[0].addEventListener('click', () => {
+  document.getElementsByClassName('qestion-card_whois')[0].classList.toggle('qestion-card_active');
+  document.getElementsByClassName('qestion-card_whois')[0].classList.toggle('qestion-card_hide');
+  document.getElementsByClassName('qestion-card_whois')[0].ontransitionend = () => {
+    document.getElementsByClassName('qestion-card_which')[0].classList.toggle('qestion-card_active');
+    document.getElementsByClassName('qestion-card_whois')[0].classList.toggle('qestion-card_hidden');
+  };
+  //document.getElementsByClassName("qestion-card_which")[0].classList.toggle("qestion-card_hidden");
+  document.getElementsByClassName('overlay')[0].classList.toggle('overlay_hidden');
+});
