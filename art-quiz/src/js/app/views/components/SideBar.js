d598e635ad901e6ae13076aa0cce79c13bbc2bcb
thirdmadman <thirdmadman@gmail.com>
2021-11-23 15:34:24 +0400

    feat: rebuild sidebar component after set data

--- art-quiz/src/js/app/views/components/SideBar.js
+++ art-quiz/src/js/app/views/components/SideBar.js
@@ -2,85 +2,84 @@ class SideBar {
   constructor(data) {
     this.data = data;
     this.isHidden = false;
+
+    this.rootEl = document.createElement('div');
+    this.rootEl.classList.add('sidebar');
+
+    this.sidebarContainer = document.createElement('div');
+    this.sidebarContainer.classList.add('sidebar__container');
+
+    this.buttonClose = document.createElement('div');
+    this.buttonClose.classList.add('button-close');
+
+    this.sidebarVerticalSeparator = document.createElement('div');
+    this.sidebarVerticalSeparator.classList.add('sidebar__vertical-separator');
+
+    this.mainMenuList = document.createElement('div');
+    this.mainMenuList.classList.add('main-menu__list');
+
+    this.fastLangSwitch = document.createElement('div');
+    this.fastLangSwitch.classList.add('main-menu__fast-lang-sw', 'fast-lang-sw');
+
+
+    this.rootEl.append(this.sidebarContainer);
+
+
+
   }
 
   setData(data) {
     this.data = data;
-  }
+    console.log(this.data);
 
-  assembly(part) {
-    switch (part) {
-      case 'fastLangSwitch': {
-        this.fastLangSwitch = document.createElement('div');
-        this.fastLangSwitch.classList.add('main-menu__fast-lang-sw', 'fast-lang-sw');
-    
-        this.data.fastlangsw.forEach((el) => {
-          let button = document.createElement('button');
-          button.classList.add('fast-lang-sw__lang-button');
-          button.textContent = el.text;
-          button.onclick = el.action;
-          this.fastLangSwitch.append(button);
-        });
-        return this.fastLangSwitch
-      }
-      case 'buttonClose': {
-        this.buttonClose = document.createElement('div');
-        this.buttonClose.classList.add('button-close');
-        return this.buttonClose;
-      }
-      case 'mainMenuList': {
-        this.mainMenuList = document.createElement('div');
-        this.mainMenuList.classList.add('main-menu__list');
-    
-        this.data.menu.forEach((el) => {
-          let button = document.createElement('button');
-          button.classList.add('main-menu__list-button');
-          button.textContent = el.text;
-          button.onclick = el.action;
-          this.mainMenuList.append(button);
-        });
-        return this.mainMenuList;
-      }
-      case 'sidebarVerticalSeparator': {
-        this.sidebarVerticalSeparator = document.createElement('div');
-        this.sidebarVerticalSeparator.classList.add('sidebar__vertical-separator');
-        let sidebarVerticalSeparatorContent = document.createElement('span');
-        sidebarVerticalSeparatorContent.innerText = this.data.separatortext; 
-        for (let i = 0; i < 10; i++) this.sidebarVerticalSeparator.append(sidebarVerticalSeparatorContent.cloneNode(true));
-        return this.sidebarVerticalSeparator;
-      }
-      
-    }
-
-  }
-
-  render() {
-
-    this.element = document.createElement('div');
-    this.element.classList.add('sidebar');
-
-    let sidebarContainer = document.createElement('div');
-    sidebarContainer.classList.add('sidebar__container');
+    this.sidebarContainer.innerHTML = "";
 
     this.mainMenu = document.createElement('div');
     this.mainMenu.classList.add('sidebar__main-menu', 'main-menu');
 
-    this.mainMenu.append(this.assembly('buttonClose'));
-    this.mainMenu.append(this.assembly('mainMenuList'));
-    this.mainMenu.append(this.assembly('fastLangSwitch'));
-    sidebarContainer.appendChild(this.assembly('sidebarVerticalSeparator'));
-    sidebarContainer.appendChild(this.mainMenu);
-    this.element.append(sidebarContainer);
+    this.mainMenuList.innerHTML = "";
 
-    return this.element;
+    this.data.menu.forEach((el) => {
+      let button = document.createElement('button');
+      button.classList.add('main-menu__list-button');
+      button.textContent = el.text;
+      button.onclick = el.action;
+      this.mainMenuList.append(button);
+    });
+
+    this.sidebarVerticalSeparator.innerHTML = "";
+
+    let sidebarVerticalSeparatorContent = document.createElement('span');
+    sidebarVerticalSeparatorContent.innerText = this.data.separatortext; 
+    for (let i = 0; i < 10; i++) this.sidebarVerticalSeparator.append(sidebarVerticalSeparatorContent.cloneNode(true));
+
+    this.fastLangSwitch.innerHTML = "";
+
+    this.data.fastlangsw.forEach((el) => {
+      let button = document.createElement('button');
+      button.classList.add('fast-lang-sw__lang-button');
+      button.textContent = el.text;
+      button.onclick = el.action;
+      this.fastLangSwitch.append(button);
+    });
+
+    this.mainMenu.append(this.buttonClose);
+    this.mainMenu.append(this.mainMenuList);
+    this.mainMenu.append(this.fastLangSwitch);
+    this.sidebarContainer.appendChild(this.sidebarVerticalSeparator);
+    this.sidebarContainer.appendChild(this.mainMenu);
+    
+  }
+  render() {
+    return this.rootEl;
   }
 
   hide() {
-    this.element.classList.add('sidebar_hidden');
+    this.rootEl.classList.add('sidebar_hidden');
     this.isHidden = true;
   }
   show() {
-    this.element.classList.remove('sidebar_hidden');
+    this.rootEl.classList.remove('sidebar_hidden');
     this.isHidden = false;
   }
 }
