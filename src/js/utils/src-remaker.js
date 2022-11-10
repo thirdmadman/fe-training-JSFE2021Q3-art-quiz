8c6d29c6fe767978a61e8c40c78342f1a8ab5a7c
thirdmadman <thirdmadman@gmail.com>
2022-11-10 22:42:02 +0600

    refactor: lint file

--- src/js/utils/src-remaker.js
+++ src/js/utils/src-remaker.js
@@ -1,32 +1,33 @@
+/* eslint-disable no-console */
 const translate = require('@vitalets/google-translate-api');
 const imagesData = require('./assets/json/images.json');
 const fs = require('fs');
-const {resolve} = require('path');
+const { resolve } = require('path');
 
 const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 const createTranslation = (imagesDataArray) => {
-  let outputPromise = Promise.all(
+  const outputPromise = Promise.all(
     imagesDataArray.map(async (el) => {
-      let result = {...el};
-      let translationObj = await translate(`${el.author}: ${el.name}`, {from: 'ru', to: 'en'});
+      const result = { ...el };
+      const translationObj = await translate(`${el.author}: ${el.name}`, { from: 'ru', to: 'en' });
       const translationSplittedArray = translationObj.text.split(': ');
-      let translationAuthor = translationSplittedArray[0];
-      let translationName = capitalizeFirstLetter(translationSplittedArray[1]);
+      const translationAuthor = translationSplittedArray[0];
+      const translationName = capitalizeFirstLetter(translationSplittedArray[1]);
 
-      result.author = {en: translationAuthor, ru: el.author};
-      result.name = {en: translationName, ru: el.name};
+      result.author = { en: translationAuthor, ru: el.author };
+      result.name = { en: translationName, ru: el.name };
       return result;
-    })
+    }),
   );
   return outputPromise;
 };
 
 const finalPath = resolve(__dirname, './assets/json/');
 createTranslation(imagesData.data).then((result) => {
-  fs.writeFile(finalPath + '/imagesTranslated.json', JSON.stringify({data: result}), (err) => {
+  fs.writeFile(`${finalPath}/imagesTranslated.json`, JSON.stringify({ data: result }), (err) => {
     if (err) throw err;
     console.log('Data written to file');
   });
@@ -35,4 +36,4 @@ createTranslation(imagesData.data).then((result) => {
 
 const imageDirPath = resolve(__dirname, './assets/img/jpg/full/');
 const files = fs.readdirSync(imageDirPath);
-files.forEach((file) => fs.rename(imageDirPath + `/${file}`, imageDirPath + `/${file.replace('full', '')}`, (err) => console.log(err)));
+files.forEach((file) => fs.rename(`${imageDirPath}/${file}`, `${imageDirPath}/${file.replace('full', '')}`, (err) => console.log(err)));
