bb7eb07a80e2fb1490039390e893b4f2702e1119
thirdmadman <thirdmadman@gmail.com>
2022-11-10 23:03:05 +0600

    refactor: correct typo in numbers

--- src/js/app/views/components/QuestionsNumbersList.js
+++ src/js/app/views/components/QuestionsNumbersList.js
@@ -1,26 +1,26 @@
 export default class QuestionsNumbersList {
   constructor() {
     this.rootEl = document.createElement('div');
-    this.rootEl.classList.add('questions-numers-list');
+    this.rootEl.classList.add('questions-numbers-list');
   }
 
   setData(levelModel) {
     this.rootEl.innerHTML = '';
     levelModel.getQuestions().forEach((question, i, array) => {
       const questionIcon = document.createElement('div');
-      questionIcon.classList.add('questions-numers-list__item');
+      questionIcon.classList.add('questions-numbers-list__item');
 
       const questionNumber = document.createElement('p');
       questionNumber.innerText = question.getNumber();
 
       if (question.getUserAnswer()) {
         if (question.getIsUserAnswerCorrect()) {
-          questionIcon.classList.add('questions-numers-list__item_correct');
+          questionIcon.classList.add('questions-numbers-list__item_correct');
         } else {
-          questionIcon.classList.add('questions-numers-list__item_incorrect');
+          questionIcon.classList.add('questions-numbers-list__item_incorrect');
         }
       } else if (((array[i - 1] && array[i - 1].userAnswer) || i === 0) && !(array[i + 1] && array[i + 1].userAnswer)) {
-        questionIcon.classList.add('questions-numers-list__item_active');
+        questionIcon.classList.add('questions-numbers-list__item_active');
       }
 
       questionIcon.append(questionNumber);
